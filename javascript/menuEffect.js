61f587697eac2834bfc30fc0910f60e8a0b181a9
Johan <131137640+Johan025@users.noreply.github.com>
2023-07-17 19:45:44 +0200

    Add files via upload

--- javascript/menuEffect.js
+++ javascript/menuEffect.js
@@ -1,10 +1,10 @@
 const menu=document.querySelector('.icone_menu');
 const nav=document.querySelector('.header_contain');
 const header=document.querySelector('#header');
+const ul=document.querySelector('#ul');
 
 menu.addEventListener('click', ()=>{
-nav.classList.toggle('hideshow');
-header.style.zIndex='100';
-header.classList.toggle('zIndex');
-menu.classList.add('changecolor');
+        nav.classList.toggle('hideshow');
+        header.style.zIndex='100';   
+        ul.classList.toggle('h2');
 });
--- javascript/withouyActualisation.js
+++ javascript/withouyActualisation.js
@@ -1,13 +1,42 @@
-// document.getElementById("form").addEventListener("submit", function(event){
-// event.preventDefault();
-//  var ville=document.querySelector(".button_hotel");
+const item=document.querySelector('.in_1');
+const item1=document.querySelector('.in_2');
+const item2=document.querySelector('.in_3');
 
-//  var xhr=new XMLHttpRequest();
-// xhr.open("get", "", true);
-// xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-//  xhr.onreadystatechange= function() {
-//  if (xhr.readyState==XMLHttpRequest.DONE &&xhr.status==200){
-//       document.getElementById("affichage");
-//       }
-//  }
-// });
+item.style.cursor="pointer";
+item1.style.cursor="pointer";
+item2.style.cursor="pointer";
+
+item.addEventListener('mouseover', ()=>{
+      item.style.marginTop = "-11.2rem";
+      item.style.transition="All 0.8s";
+
+})
+
+item.addEventListener('mouseout', ()=>{
+    item.style.marginTop = "-4.1rem";
+    item.style.transition="All 0.4s";
+})
+
+//item1
+item1.addEventListener('mouseover', ()=>{
+    item1.style.marginTop = "-11.2rem";
+    item1.style.transition="All 0.8s";
+
+})
+
+item1.addEventListener('mouseout', ()=>{
+  item1.style.marginTop = "-4.1rem";
+  item1.style.transition="All 0.4s";
+})
+
+//item2
+item2.addEventListener('mouseover', ()=>{
+    item2.style.marginTop = "-11.2rem";
+    item2.style.transition="All 0.8s";
+
+})
+
+item2.addEventListener('mouseout', ()=>{
+  item2.style.marginTop = "-4.1rem";
+  item2.style.transition="All 0.4s";
+})
